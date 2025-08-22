27612c985e32e831ff5248d7dde98cf8b074b913
viswa2137 <vgorripati2s@semo.edu>
2025-08-22 14:42:52 -0500

    last

---
+++ treeset.java
@@ -0,0 +1,16 @@
+package collections;
+import java.util.*;
+public class treeset {
+
+	public static void main(String[] args) {
+		TreeSet<String> ts = new TreeSet<>();
+
+        ts.add("pavan");
+        ts.add("pavan");
+        ts.add("vishnu");
+        ts.add("ram"); 
+        System.out.println(ts);
+
+	}
+
+}
